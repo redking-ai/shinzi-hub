54f4e2b252457fa3fa9163b27fe2ad0ba54e2edf
Red king <sboficial226@gmail.com>
2026-09-20 22:49:59 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCTYugucY0DXU2tTwAmfX3sA_y_KSs5VGA",
@@ -11,5 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 
+export const auth = getAuth(app);
+export const db = getFirestore(app);
